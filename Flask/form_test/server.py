05d8bc6fca3d01d14d6646f9b4cbad12152504d3
arvisionz <ashwin.raman@gmail.com>
2017-10-16 22:58:20 -0500

    Added Counter

---
+++ Flask/form_test/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+# our index route will handle rendering our form
+app.secret_key = 'ThisIsSecret'
+@app.route('/')
+def index():
+  return render_template("index.html")
+# this route will handle our form submission
+# notice how we defined which HTTP methods are allowed by this route
+@app.route('/users', methods=['POST'])
+def create_user():
+   print "Got Post Info"
+   # here we add two properties to session to store the name and email
+   session['name'] = request.form['name']
+   session['email'] = request.form['email']
+   return redirect('/show') # noticed that we changed where we redirect to so that we can go to the page that displays the name and email!
+@app.route('/show')
+def show_user():
+  return render_template('user.html')
+app.run(debug=True) # run our server
