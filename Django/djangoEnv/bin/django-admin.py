1cdab6d4611e6002f247251121e6533b91e814cf
arvisionz <ashwin.raman@gmail.com>
2017-11-18 22:36:16 -0600

    Django Stuff

---
+++ Django/djangoEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mbp/Desktop/CodingDojo/DojoAssignments/Python/Django/djangoEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
